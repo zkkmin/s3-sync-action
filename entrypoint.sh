abfe83695612d436c51108bdb7f33e87164f1ca3
Franck Abgrall <abgrallkefran@gmail.com>
2020-04-27 19:46:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,11 @@ if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
   exit 1
 fi
 
+if [ -z "$DISTRIBUTION_ID" ]; then
+  echo "DISTRIBUTION_ID is not set. Quitting."
+  exit 1
+fi
+
 # Default to us-east-1 if AWS_REGION not set.
 if [ -z "$AWS_REGION" ]; then
   AWS_REGION="us-east-1"
