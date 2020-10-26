7cc0da052cec90df2783bf7b12dec86d7778b175
ZIN KO KO MIN <zkkmin@gmail.com>
2020-10-26 20:34:29 +0800

    Update entrypoint.sh
    Remove --path option since I don't need it.

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,7 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
               
-sh -c "aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*" --profile s3-sync-action"
+sh -c "aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --profile s3-sync-action"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
